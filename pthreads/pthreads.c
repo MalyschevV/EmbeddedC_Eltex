94133104156033e62f24d3cf827f71da3288e8d0
Vladimir <malyschevvvv@mail.ru>
2023-07-12 18:28:10 +0700

    add pthread lection example

---
+++ pthreads/pthreads.c
@@ -0,0 +1,23 @@
+#include <pthread.h>
+#include <stdio.h>
+#define THREADS 5
+
+void *ThreadCalc(void *args) {
+  int *ptr = (int *)args;
+  printf("%d\n", *ptr);
+  return NULL;
+}
+
+int main() {
+  int thread_array[THREADS];
+  int *ptr = NULL;
+  pthread_t thread[THREADS];
+  for (int i = 0; i < THREADS; i++) {
+    thread_array[i] = i;
+    pthread_create(&thread[i], NULL, ThreadCalc, (void *)&thread_array[i]);
+  }
+  for (int i = 0; i < THREADS; i++) {
+    pthread_join(thread[i], (void **)&ptr);
+  }
+  return 0;
+}
