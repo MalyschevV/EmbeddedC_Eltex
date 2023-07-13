9a0f9e2fc8e7c49a22c1341266d2912cae1f0b11
Vladimir <malyschevvvv@mail.ru>
2023-07-13 19:22:15 +0700

    add Makefiles for thread tasks

--- pthreads/task1/pthreads.c
+++ pthreads/task1/pthreads.c
@@ -12,12 +12,15 @@ int main() {
   int thread_array[THREADS];
   int *ptr = NULL;
   pthread_t thread[THREADS];
+
   for (int i = 0; i < THREADS; i++) {
     thread_array[i] = i;
     pthread_create(&thread[i], NULL, ThreadCalc, (void *)&thread_array[i]);
   }
+
   for (int i = 0; i < THREADS; i++) {
     pthread_join(thread[i], (void **)&ptr);
   }
+
   return 0;
 }
--- pthreads/task2/threadrace.c
+++ pthreads/task2/threadrace.c
@@ -6,11 +6,8 @@
 long sum = 0;
 
 void *ThreadRace(void *args) {
-  long tmp;
   for (int i = 0; i < VALUE; i++) {
-    tmp = sum;
-    tmp++;
-    sum = tmp;
+    sum++;
   }
 }
 
@@ -18,13 +15,17 @@ int main() {
   pthread_t thread_array[THREADS];
   long *ptr = NULL;
   long array[THREADS];
+
   for (int i = 0; i < THREADS; i++) {
     array[i] = i;
-    pthread_create(&thread_array[i], NULL, ThreadRace, &array[i]);
+    pthread_create(&thread_array[i], NULL, ThreadRace, (void*)&array[i]);
   }
+
   for (int i = 0; i < THREADS; i++) {
     pthread_join(thread_array[i], (void **)&ptr);
   }
+
   printf("sum = %ld\n", sum);
+  
   return 0;
 }
